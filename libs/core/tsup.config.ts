1f0f2f5da1df947a661e53e75fe2f32aaaa03ff0
homveloper <bum4496@naver.com>
2026-01-31 21:57:43 +0900

    fix(core): correct tsup output directory path

--- libs/core/tsup.config.ts
+++ libs/core/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['libs/core/src/index.ts'],
-  outDir: 'dist/libs/core',
+  outDir: '../../dist/libs/core',
   tsconfig: 'libs/core/tsconfig.lib.json',
   format: ['cjs', 'esm'],
   dts: true,
