9fb9dc8cbc038a1b06518e52f354637c8255b2dd
homveloper <bum4496@naver.com>
2026-02-14 17:10:58 +0900

    chore: remove root obsolete files

--- inspect_elk_worker.js
+++
@@ -1,4 +0,0 @@
-const workerLib = require('./node_modules/elkjs/lib/elk-worker.min.js');
-console.log('Type of export:', typeof workerLib);
-console.log('Worker property:', workerLib.Worker);
-console.log('Keys:', Object.keys(workerLib));
